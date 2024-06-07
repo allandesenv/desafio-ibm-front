1cfad460a9e56d87751e1d4b4f448d4fe7040afc
allandesenv <allandd8@gmail.com>
2024-06-07 15:37:13 -0300

    Initial Commit

--- desafio-ibm-app/src/app/app.component.ts
+++ desafio-ibm-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isMenuOpen: boolean = true; // Variável de controle do estado do menu
+  isMenuOpen: boolean = false; // Variável de controle do estado do menu
+  isMobile: boolean = false; // Variável de controle do estado mobile
+
+  constructor() {
+    this.detectMobile(); // Chama a função para detectar se está no modo mobile
+  }
+
+  detectMobile() {
+    // Lógica para detectar se está no modo mobile
+    const screenWidth = window.innerWidth;
+    this.isMobile = screenWidth <= 600; // Defina isMobile como verdadeiro se a largura da tela for menor ou igual a 600px
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.detectMobile(); // Chama a função para detectar se está no modo mobile ao redimensionar a janela
+    if (!this.isMobile) {
+      this.isMenuOpen = true; // Garante que o menu esteja aberto no modo desktop
+    } else {
+      this.isMenuOpen = false; // Garante que o menu esteja fechado no modo mobile
+    }
+  }
 
   toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen; // Método para alternar o estado do menu
+    this.isMenuOpen = !this.isMenuOpen; // Alterna o estado do menu
   }
 }
