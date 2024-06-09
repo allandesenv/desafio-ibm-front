36930ce75b15db056099278ac7900734d1f71131
allandesenv <allandd8@gmail.com>
2024-06-09 15:00:23 -0300

    Initial Commit

--- desafio-ibm-app/src/app/pages/debito/debito.component.ts
+++ desafio-ibm-app/src/app/pages/debito/debito.component.ts
@@ -27,18 +27,28 @@ export class DebitoComponent implements OnInit {
   }
 
   onSubmit() {
-    this.debitoService.addDebito(this.operacao.id_cliente, this.operacao.valor).subscribe(
-      response => {
-        this.mensagem = 'Débito adicionado com sucesso!';
-        this.isSuccess = true;
-        this.resetForm();
-      },
-      error => {
-        this.mensagem = 'Erro ao adicionar débito. Tente novamente.';
-        this.isSuccess = false;
-        this.resetForm();
-      }
-    );
+    // Verifica se o cliente foi selecionado e se o valor é maior que zero
+    if (this.operacao.id_cliente && this.operacao.valor > 0) {
+      this.debitoService.addDebito(this.operacao.id_cliente, this.operacao.valor).subscribe(
+        response => {
+          this.mensagem = 'Débito adicionado com sucesso!';
+          this.isSuccess = true;
+          this.resetForm();
+        },
+        error => {
+          if (error.error && error.error.message) {
+            this.mensagem = 'Erro ao adicionar débito: ' + error.error.message;
+          } else {
+            this.mensagem = 'Erro ao adicionar débito. Tente novamente.';
+          }
+          this.isSuccess = false;
+          this.resetForm();
+        }
+      );
+    } else {
+      // Exibe mensagem de erro se o cliente não estiver selecionado ou se o valor for zero ou menor
+      this.mensagem = 'Selecione um cliente e insira um valor maior que zero.';
+    }
   }
 
   resetForm() {
